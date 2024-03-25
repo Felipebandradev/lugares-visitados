832e7db522a4d9c94c6d7c7f0f17a438b08307b2
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-03-25 11:58:08 -0300

    feat: estruturação da página

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Pressable,
+  Image,
+  ScrollView,
+} from "react-native";
+
+import imagemBase from "./assets/images/imagemBase.jpg";
+import MapView from "react-native-maps";
+import { useState } from "react";
 
 export default function App() {
+  const [localizacao, setLocalizacao] = useState(null);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar color="#f7f7f7" />
+      <View style={estilos.container}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Text> Fotos de lugares visitados</Text>
+          <View style={estilos.verFoto}>
+            <Image
+              resizeMode="contain"
+              style={estilos.fotoLocal}
+              source={imagemBase}
+            />
+            <Pressable style={estilos.botao}>
+              <Text style={estilos.textoBotao}>Tirar Foto</Text>
+            </Pressable>
+          </View>
+          <View style={estilos.verLocal}>
+            {localizacao ? (
+              <MapView />
+            ) : (
+              <Image
+                resizeMode="contain"
+                style={estilos.fotoLocal}
+                source={imagemBase}
+              />
+            )}
+
+            <Pressable style={estilos.botao}>
+              <Text style={estilos.textoBotao}>Localizar no Mapa</Text>
+            </Pressable>
+          </View>
+        </ScrollView>
+      </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    marginVertical: 18,
+  },
+  fotoLocal: {
+    width: 300,
+    height: 250,
+    marginVertical: 18,
   },
 });
