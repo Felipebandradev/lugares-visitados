9e3d74a29e1f25903b50c42dfd9bb9596152f8f1
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-03-26 11:47:06 -0300

    feat: Estilização básica do projeto e todos os recursos funcionando

--- App.js
+++ App.js
@@ -7,10 +7,13 @@ import {
   Image,
   ScrollView,
   Alert,
+  TextInput,
 } from "react-native";
 
-import imagemBase from "./assets/images/imagemBase.jpg";
-import MapView from "react-native-maps";
+import imagemBase from "./assets/images/imagemBase.png";
+import mapaBase from "./assets/images/mapaBase.png";
+import marcadorEstrela from "./assets/images/marcadorEstrela.png";
+import MapView, { Marker } from "react-native-maps";
 import { useEffect, useState } from "react";
 /* Importando o imagePicker */
 import * as ImagePicker from "expo-image-picker";
@@ -51,36 +54,70 @@ export default function App() {
     Obterpermissoes();
   }, []);
 
-  const tirarFotoLocal = async () => {};
+  const tirarFotoLocal = async () => {
+    const foto = await ImagePicker.launchCameraAsync({
+      allowsEditing: false,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+
+    if (!foto.canceled) {
+      setCamera(foto.assets[0].uri);
+    }
+  };
+
+  const melocaliza = () => {
+    setLocalizacao({
+      latitude: euMapa.coords.latitude,
+      longitude: euMapa.coords.longitude,
+
+      latitudeDelta: 0.001,
+      longitudeDelta: 0.002,
+    });
+  };
 
   return (
     <>
       <StatusBar color="#f7f7f7" />
       <View style={estilos.container}>
         <ScrollView showsVerticalScrollIndicator={false}>
-          <Text> Fotos de lugares visitados</Text>
-          <View style={estilos.verFoto}>
+          <Text style={estilos.titulo}> Fotos de lugares visitados</Text>
+          <TextInput
+            style={estilos.campoNomeLugar}
+            placeholder="Digite o nome do local"
+          />
+
+          <View style={estilos.fotoca}>
             <Image
-              resizeMode="contain"
               style={estilos.fotoLocal}
-              source={imagemBase}
+              source={camera ? { uri: camera } : imagemBase}
             />
             <Pressable onPress={tirarFotoLocal} style={estilos.botao}>
               <Text style={estilos.textoBotao}>Tirar Foto</Text>
             </Pressable>
           </View>
+
           <View style={estilos.verLocal}>
             {localizacao ? (
-              <MapView />
-            ) : (
-              <Image
-                resizeMode="contain"
+              <MapView
                 style={estilos.fotoLocal}
-                source={imagemBase}
-              />
+                mapType="hybrid"
+                region={localizacao}
+                scrollEnabled={false}
+              >
+                <Marker coordinate={localizacao}>
+                  <Image
+                    resizeMode="contain"
+                    style={estilos.marcardor}
+                    source={marcadorEstrela}
+                  />
+                </Marker>
+              </MapView>
+            ) : (
+              <Image style={estilos.fotoLocal} source={mapaBase} />
             )}
 
-            <Pressable style={estilos.botao}>
+            <Pressable onPress={melocaliza} style={estilos.botao}>
               <Text style={estilos.textoBotao}>Localizar no Mapa</Text>
             </Pressable>
           </View>
@@ -94,12 +131,46 @@ const estilos = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     marginVertical: 18,
+    alignItems: "center",
   },
   fotoLocal: {
-    width: 300,
-    height: 250,
+    width: 350,
+    height: 300,
     marginVertical: 18,
+    borderRadius: 5,
+  },
+  marcardor: {
+    width: 60,
+    height: 60,
+  },
+  botao: {
+    backgroundColor: "#F1B215",
+    padding: 12,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 5,
+  },
+  textoBotao: {
+    color: "#f7f7f7",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  campoNomeLugar: {
+    borderColor: "#F1B215",
+    padding: 12,
+    borderWidth: 3,
+    borderRadius: 5,
+    marginVertical: 12,
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  titulo: {
+    textAlign: "center",
+    fontSize: 20,
+    color: "#f7f7f7",
+    backgroundColor: "#F1B215",
+    padding: 12,
+    borderRadius: 5,
   },
 });
